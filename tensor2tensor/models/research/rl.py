72e55d11f50a3bf699e6d0e4bba24c5783ece7df
piotrmilos <piotr.milos@codilime.com>
2018-02-25 11:29:37 +0100

    Adding bigger cnn net

--- tensor2tensor/models/research/rl.py
+++ tensor2tensor/models/research/rl.py
@@ -140,14 +140,45 @@ def feed_forward_cnn_small_categorical_fun(action_space, config, observations):
 
   with tf.variable_scope('policy'):
     x = tf.to_float(x)/255.0
-    x = tf.contrib.layers.conv2d(x, 32, [5, 5], [2, 2], activation_fn= tf.nn.relu, padding="SAME")
-    x = tf.contrib.layers.conv2d(x, 32, [5, 5], [2, 2], activation_fn=tf.nn.relu, padding="SAME")
+    x = tf.contrib.layers.conv2d(x, num_outputs=32, kernel_size=[5, 5],
+                                 stride=[2, 2], activation_fn= tf.nn.relu, padding="SAME")
+    x = tf.contrib.layers.conv2d(x, num_outputs=32, kernel_size=[5, 5],
+                                 stride=[2, 2], activation_fn=tf.nn.relu, padding="SAME")
 
     flat_x = tf.reshape(x, [
       tf.shape(observations)[0], tf.shape(observations)[1],
       functools.reduce(operator.mul, x.shape.as_list()[1:], 1)])
 
     x = tf.contrib.layers.fully_connected(flat_x, 128, tf.nn.relu)
+    logits = tf.contrib.layers.fully_connected(x, action_space.n, activation_fn=None)
+
+    value = tf.contrib.layers.fully_connected(x, 1, activation_fn=None)[..., 0]
+    policy = tf.contrib.distributions.Categorical(logits=logits)
+
+  return NetworkOutput(policy, value, lambda a: a)
+
+
+def feed_forward_cnn_categorical_fun(action_space, config, observations):
+  """Big categorical network. Based on
+  https://github.com/openai/baselines/commit/
+  9fa8e1baf1d1f975b87b369a8082122eac812eb1#diff-5d7291e9f275af3eaad4d4cbfec2c9e0"""
+  obs_shape = observations.shape.as_list()
+  x = tf.reshape(observations, [-1]+ obs_shape[2:])
+
+  with tf.variable_scope('policy'):
+    x = tf.to_float(x)/255.0
+    x = tf.contrib.layers.conv2d(x, num_outputs=32, kernel_size=[8, 8],
+                                 stride=[4, 4], activation_fn= tf.nn.relu, padding="SAME")
+    x = tf.contrib.layers.conv2d(x, num_outputs=64, kernel_size=[4, 4],
+                                 stride=[2, 2], activation_fn=tf.nn.relu, padding="SAME")
+    x = tf.contrib.layers.conv2d(x, num_outputs=64, kernel_size=[3, 3],
+                                 stride=[1, 1], activation_fn=tf.nn.relu, padding="SAME")
+
+    flat_x = tf.reshape(x, [
+      tf.shape(observations)[0], tf.shape(observations)[1],
+      functools.reduce(operator.mul, x.shape.as_list()[1:], 1)])
+
+    x = tf.contrib.layers.fully_connected(flat_x, 512, tf.nn.relu)
     logits = tf.contrib.layers.fully_connected(x, action_space.n, activation_fn=None)
 
     value = tf.contrib.layers.fully_connected(x, 1, activation_fn=None)[..., 0]
